d9a0276bb2cfd72ef38bd8eece16e2f00ac18b16
Souvik <souvikmaity2003@gmail.com>
2025-03-30 22:19:43 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Souvik Maity',
+    author_email= 'souvikmaity147@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
